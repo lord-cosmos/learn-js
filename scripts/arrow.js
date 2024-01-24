73ea79e730cb8eb0302cba11dbee200fad45d4d4
lord-cosmos <teslaatozinspires7@gmail.com>
2024-01-24 15:47:29 -0500

    fix error - arrow.js

--- scripts/arrow.js
+++ scripts/arrow.js
@@ -1,9 +1,13 @@
 const person = {
- firstName: 'Grace',
- lastName: 'Hopper',
- age: 89,
- fullName: () => { return this.firstName + ' ' + this.lastName }
-}
+  firstName: "Grace",
+  lastName: "Hopper",
+  age: 89,
+  // the error was : firstName and lastName is not defined in the person's lexical scope.
+  // this happens only in the case of "arrow-functions"
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
 
 let x = person.fullName();
 console.log(x);
