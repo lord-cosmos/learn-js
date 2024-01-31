1fd1e4df384c13d670576b997d600a113de7f72b
lord-cosmos <teslaatozinspires7@gmail.com>
2024-01-31 15:55:50 -0500

    anonymize emails

--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -1,11 +1,27 @@
 /* anonymizes the username(local part) of an email ID, that is, 
 replace the username part with * characters.
 */
-const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.edu', 'jk@neu.edu', 'jsned@', 'ai_me@mugar.northeastern.edu'];
-window.onload = function hideEmail() {
-    const list = document.getElementById('emails')
+
+const emails = [
+    'mary@northeastern.edu',
+    'ari@khoury.northeastern.edu',
+    'jk_neu.edu',
+    'jk@neu.edu',
+    'jsned@',
+    'ai_me@mugar.northeastern.edu'
+  ];
+  
+  window.onload = function hideEmail() {
+    const list = document.getElementById('emails');
     list.innerHTML = '';
+  
     for (const email of emails) {
-        // complete the loop
+      if (email.match(/^[a-zA-Z0-9_]+@(.+\.)?northeastern\.edu$/)) {
+        console.log(email);
+        const listItem = document.createElement("li");
+        listItem.textContent = email.replace(/^[^@]+/, (match) => '*'.repeat(match.length));
+        list.appendChild(listItem);
+      }
     }
-}
+  };
+  
